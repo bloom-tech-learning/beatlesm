1e83dceb44fd998c6010ae634ed118b1b0f83976
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-25 13:03:54 -0700

    build App.js function

--- curriculum/web48/3.1-Advanced-React/Module_1-Class-Components/Guided311/review/src/App.js
+++ curriculum/web48/3.1-Advanced-React/Module_1-Class-Components/Guided311/review/src/App.js
@@ -1,14 +1,32 @@
 import React, {useState} from "react";
 
+const Person = (props) => {
+  console.log('props:', props);
+  return (
+    <div>
+      <h1>Hello {props.name}, you are age of {props.age}.</h1>
+      <p>Hey {props.name}! {props.name} is a great name, don't change it!</p>
+    </div>
+  );
+}
+
 const App = () => {
-  const [name, setName] = useState("Petre");
+
+  const [state, setState] = useState({
+    name: "Alison",
+    age: 34
+  });
+
+  const handleClick = () => {
+    setState({
+      ...state,
+      name: "someone"});
+  }
 
   return (
     <div>
-      <h1>Hello {name}</h1>
-      <button onClick = { () => {
-        setName("Soof");
-      }}>Change Name</button>
+      <Person name = {state.name} age= {state.age}/>      
+      <button onClick = { handleClick}>Change Name</button>
     </div>
   );
 };
