6056ffecba514656cff36cd85cfea42d38f9f490
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-25 12:33:45 -0700

    button click ok

--- curriculum/web48/3.1-Advanced-React/Module_1-Class-Components/Guided311/review/src/App.js
+++ curriculum/web48/3.1-Advanced-React/Module_1-Class-Components/Guided311/review/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 const App = () => {
+  const [name, setName] = useState("Petre");
+
   return (
     <div>
-      <h1>Hello Warren</h1>
+      <h1>Hello {name}</h1>
+      <button onClick = { () => {
+        setName("Soof");
+      }}>Change Name</button>
     </div>
   );
 };
