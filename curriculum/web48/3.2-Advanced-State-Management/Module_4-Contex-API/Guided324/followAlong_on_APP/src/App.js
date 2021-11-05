74f1479bc426a1eb766d7f0c32a38b673c032a33
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-04 18:21:55 -0700

    add another solution to guided 3.2.4

---
+++ curriculum/web48/3.2-Advanced-State-Management/Module_4-Contex-API/Guided324/followAlong_on_APP/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, createContext } from "react";
+
+import { data } from "./data";
+
+import FamilyTree from "./components/FamilyTree";
+import "./styles.scss";
+
+import FamilyContext from "./contexts/FamilyContext";
+
+export default function App() {
+  const [families] = useState(data);
+  const [activeFamily, setActiveFamily] = useState(families[0]);
+
+  return (
+    <div className="App">
+      <section className="header">
+        <h1>Family Trees</h1>
+        {families.map(family => (
+          <button
+            className={`family-button ${family.familyName ===
+              activeFamily.familyName && "active"}`}
+            key={family.familyName}
+            onClick={() => setActiveFamily(family)}
+          >
+            {family.familyName}
+          </button>
+        ))}
+      </section>
+      <FamilyContext.Provider value = {activeFamily} > 
+        {activeFamily && <FamilyTree  />}
+      </FamilyContext.Provider>      
+    </div>
+  );
+}
